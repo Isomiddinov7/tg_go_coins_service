6451aeb35f1f9543e497030369a5e1b9d6c35cdf
Isomiddinov Bahodir <isomiddinovbahodir7@gmail.com>
2024-09-09 22:43:23 +0500

    done

--- storage/postgres/history.go
+++ storage/postgres/history.go
@@ -24,16 +24,18 @@ func (r *historyRepo) HistoryUser(ctx context.Context, req *coins_service.Histor
 		resp  coins_service.HistoryUserResponse
 		query = `
 			SELECT 
-				id,
-				status,
-				user_confirmation_img,
-				coin_amount,
-				coin_price,
-				all_price,
-				user_address,
-				payment_card,
-				created_at
+				ut.id,
+				c.name
+				ut.status,
+				ut.user_confirmation_img,
+				ut.coin_amount,
+				ut.coin_price,
+				ut.all_price,
+				ut.user_address,
+				ut.payment_card,
+				ut.created_at
 			FROM "user_transaction"
+			JOIN "coins" as c ON c.id = ut.coin_id
 			WHERE user_id = $1
 		`
 	)
@@ -48,6 +50,7 @@ func (r *historyRepo) HistoryUser(ctx context.Context, req *coins_service.Histor
 		var (
 			history               coins_service.HistoriesUser
 			id                    sql.NullString
+			name                  sql.NullString
 			status                sql.NullString
 			user_confirmation_img sql.NullString
 			coin_amount           sql.NullString
@@ -60,6 +63,7 @@ func (r *historyRepo) HistoryUser(ctx context.Context, req *coins_service.Histor
 
 		err = rows.Scan(
 			&id,
+			&name,
 			&status,
 			&user_confirmation_img,
 			&coin_amount,
@@ -75,8 +79,9 @@ func (r *historyRepo) HistoryUser(ctx context.Context, req *coins_service.Histor
 
 		history = coins_service.HistoriesUser{
 			Id:         id.String,
+			Name:       name.String,
 			Status:     status.String,
-			CoinImg:    user_confirmation_img.String,
+			ConfirmImg: user_confirmation_img.String,
 			CoinAmount: coin_amount.String,
 			CoinPrice:  coin_price.String,
 			AllPrice:   all_price.String,
